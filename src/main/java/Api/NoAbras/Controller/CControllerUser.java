59e3a151981fd35e7e563c396bdb89c9e4da4937
lucia <luciatiradoturiel@gmail.com>
2023-04-14 14:58:08 +0200

    first dev login

---
+++ src/main/java/Api/NoAbras/Controller/CControllerUser.java
@@ -0,0 +1,58 @@
+package Api.NoAbras.Controller;
+import Api.NoAbras.Config.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class CControllerUser {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity(new ApiResponse(false, "Username is already taken!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        Usuario user = new Usuario();
+        user.setUsername(signUpRequest.getUsername());
+        user.setPassword(signUpRequest.getPassword());
+
+        userRepository.save(user);
+
+        return ResponseEntity.created(new URI("/api/users/" + user.getId()))
+                .body(new ApiResponse(true, "User registered successfully"));
+    }
+}
